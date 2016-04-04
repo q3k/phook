b9a69ad45cf3a74e07254c16893a75223f84423a
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2016-04-04 21:22:53 +0100

    Document some public calls

--- phook.h
+++ phook.h
@@ -36,9 +36,45 @@ typedef enum {
     COULDNT_ASSEMBLE = -11
 } phook_error_t;
 
-// Forward declarations of public functions
-phook_error_t phook_fork_exec_trace(const char *command, char *const argv[], pid_t *out);
-phook_error_t phook_process_allocate(pid_t process, uint64_t size, uint64_t *out);
+/// Forward declarations of public functions
+
+// Get error string for a phook error
+// err: phook error
+// return: consr char * to error string
 const char *phook_errstr(phook_error_t err);
+
+// Spawn a new process and ptrace it. This doesn't clean up or wait for the 
+// process.
+// command: process to start, like in execv(3)
+// argv: argv for new process, like in execv(3)
+// out: pointer to resulting pid_t
+// return: error or OK
+phook_error_t phook_fork_exec_trace(const char *command, char *const argv[],
+        pid_t *out);
+
+// Allocate some RWX memory in a currently ptraced process
+// process: pid_t of process
+// size: size of buffer to allocate
+// out: pointer to address of resulting buffer in process' VMA
+// return: error or OK
+phook_error_t phook_process_allocate(pid_t process, uint64_t size,
+        uint64_t *out);
+
+// Find a instruction boundary after a certain treshold (useful for finding
+// trampoline sites so that no instructions are split)
+// process: pid_t of process
+// start_address: start of disassembly
+// min_bytes: how many bytes at minimum do we want the instructions to occupy
+// boundary: pointer to the address of resulting instruction boundary
+phook_error_t phook_find_instruction_boundary(pid_t process,
+        uint64_t start_address, uint64_t min_bytes, uint64_t *boundary);
+
+// Assemble a jump/detour
+// source: where the jump will be from
+// target: where the jump should redirect
+// out: where to write the detour, or NULL to just get len
+// len: pointer to output the resulting size of detour
+phook_error_t phook_assemble_detour(uint64_t source, uint64_t target,
+        uint8_t *out, uint64_t *len);
 
 #endif // __PHOOK_H__
